047730296b3ab6af88ce782a8775c2576a16beed
Josh Strater <jstrater@gmail.com>
2023-11-30 22:39:54 -0800

    Solutions for day 1

---
+++ 01/solution2.rb
@@ -0,0 +1,55 @@
+DIGIT_MAP = {
+  '0' => 0,
+  '1' => 1,
+  '2' => 2,
+  '3' => 3,
+  '4' => 4,
+  '5' => 5,
+  '6' => 6,
+  '7' => 7,
+  '8' => 8,
+  '9' => 9,
+
+  # 'zero' isn't valid
+  'one' => 1,
+  'two' => 2,
+  'three' => 3,
+  'four' => 4,
+  'five' => 5,
+  'six' => 6,
+  'seven' => 7,
+  'eight' => 8,
+  'nine' => 9,
+}
+
+def first_digit(raw_str)
+  raise "no first digit: #{raw_str}" if raw_str.empty?
+
+  DIGIT_MAP.each do |digit_str, digit|
+    if raw_str.start_with?(digit_str)
+      return digit
+    end
+  end
+
+  first_digit(raw_str[1...])
+end
+
+def last_digit(raw_str)
+  raise "no last digit: #{raw_str}" if raw_str.empty?
+
+  DIGIT_MAP.each do |digit_str, digit|
+    if raw_str.end_with?(digit_str)
+      return digit
+    end
+  end
+
+  last_digit(raw_str[...-1])
+end
+
+puts ARGF.each_line.map { |line|
+  firstlast = "#{first_digit(line)}#{last_digit(line)}"
+  puts line.chomp
+  puts firstlast
+  puts
+  firstlast.to_i
+}.sum
